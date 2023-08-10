a12bebcc511d313c0e4ebc2868a087604af16485
SeonBap <tjsrnr3180@gmail.com>
2023-08-10 13:01:53 +0900

    공격 충돌 이벤트 생성

--- UE5_Action_Portfolio/Source/UE5_Action_Portfolio/Weapon/Arrow.cpp
+++ UE5_Action_Portfolio/Source/UE5_Action_Portfolio/Weapon/Arrow.cpp
@@ -25,6 +25,7 @@ AArrow::AArrow()
 
 void AArrow::ArrowBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// 공격 충돌 처리
 	//OtherActor->TakeDamage(10.f, );
 
 	Destroy();
